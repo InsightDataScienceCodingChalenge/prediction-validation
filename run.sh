37eac87f9abd2f60f00d2e641660a8baa68c1cdc
anvesh <anveshkumar.perugu@gmail.com>
2018-09-08 17:07:37 -0400

    updated script

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 #Below command runs the main file from the source directory
 
 
-python ./src/main.py -input_file1_path ./input/window.txt -input_file2_path ./input/actual.txt -input_file3_path ./input/predicted.txt -output_file_path ./output/comparison.txt
+python333 ./src/main.py -input_file1_path ./input/window.txt -input_file2_path ./input/actual.txt -input_file3_path ./input/predicted.txt -output_file_path ./output/comparison.txt
